57288f6f0f9335b9c4b270f82f4b16dde3f94af9
Tomás Link <tomas.link@globalfishingwatch.org>
2026-01-19 16:44:57 -0300

    Use gfw.common.cli package

--- src/pipe_loitering/cli/main.py
+++ src/pipe_loitering/cli/main.py
@@ -2,31 +2,57 @@
 import sys
 import logging
 
-from pipe_loitering.create_raw_loitering import run_create_raw_loitering
-from pipe_loitering.merge_raw_loitering import run_merge_raw_loitering
+from gfw.common.cli import CLI, Option
+from gfw.common.cli.actions import NestedKeyValueAction
+from gfw.common.logging import LoggerConfig
+from gfw.common.cli.formatting import default_formatter
+
+from pipe_loitering.cli.commands.create_raw_loitering import CreateRawLoitering
+from pipe_loitering.cli.commands.merge_raw_loitering import MergeRawLoitering
+
+from pipe_loitering.version import __version__
+
 
 logging.basicConfig(level=logging.INFO)
 
-SUBCOMMANDS = {
-    "create_raw_loitering": run_create_raw_loitering,
-    "merge_raw_loitering": run_merge_raw_loitering,
-}
+HELP_LABELS = "Labels to audit costs over the queries."
+
+
+def run(args):
+    loitering_cli = CLI(
+        name="pipe-loitering",
+        description="Tools for creating loitering events.",
+        formatter=default_formatter(max_pos=120),
+        subcommands=[
+            CreateRawLoitering,
+            MergeRawLoitering
+        ],
+        options=[  # Common options for all subcommands.
+            Option(
+                "--labels", type=str, nargs="*", action=NestedKeyValueAction, help=HELP_LABELS
+            ),
+        ],
+        version=__version__,
+        examples=[
+        ],
+        logger_config=LoggerConfig(
+            warning_level=[
+                "apache_beam.runners.portability",
+                "apache_beam.runners.worker",
+                "apache_beam.transforms.core",
+                "apache_beam.io.filesystem",
+                "apache_beam.io.gcp.bigquery_tools",
+                "urllib3"
+            ]
+        ),
+        allow_unknown=True
+    )
+
+    return loitering_cli.execute(args)
 
 
 def main():
-    logging.info("Running %s", sys.argv)
-
-    if len(sys.argv) < 2:
-        logging.info(
-            "No subcommand specified. Run pipeline [SUBCOMMAND], where subcommand is one of %s",
-            SUBCOMMANDS.keys(),
-        )
-        exit(1)
-
-    subcommand = sys.argv[1]
-    subcommand_args = sys.argv[2:]
-
-    SUBCOMMANDS[subcommand](subcommand_args)
+    run(sys.argv[1:])
 
 
 if __name__ == "__main__":
